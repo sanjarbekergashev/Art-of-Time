41ef51caf385958e126baa9d48861947d547c8ab
sanjarbekergashev <sanjarbekk9119@gmail.com>
2024-08-12 04:11:37 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+
+// 1- ENTERANCE
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true })); //traditional  req//
+app.use(express.json()); //modren req//
+
+// 2- SESSIONS
+
+// 3- VIEWS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// 4- ROUTER
+
+export default app;
